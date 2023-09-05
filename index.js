cf43dc34902356b1cb0fb0cfb9841059c768c213
GerardoCoronel98 <tinococoronel.gerardo@gmail.com>
2023-09-04 18:18:22 -0600

    We uptdate index.js with new requierments, add README.md file and uptdate package.jason

--- index.js
+++ index.js
@@ -1,23 +1,19 @@
 const express = require('express');
 const app = express();
 
-
-/*
-Verbos HTTP: 
-
-- GET
-- POST
-- PATCH
-- PUT
-- DELETE
-
-*/
-
 app.get("/", (req, res, next) => {
     res.status(200)
-    res.send("Welcum")
+    res.send("Welcome to the new server")
+})
+//Al poner los ":" se genera una variable y un enlace
+//a una dirección que desinga el usuario
+app.get("/:name", (req, res, next) => {
+    console.log(req.params.name)
+    res.status(200)
+    res.send("Hola, " + req.params.name)
+
 })
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('Server is running...')
-});
+})
